04bad35cf0b8d7bf618421a84997d4c4af42b914
Po-jui <chenborui1@gmail.com>
2024-07-24 18:04:10 +0800

    exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	total = 0
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  total = 1
+  for (let i = 0; i < array.length; i++) {
+    total *= array[i];
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1) {
+    return 1
+  }
+  let sum = 1
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      sum *= i
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/ /g,'');
+    string = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+  
+
+    let middle = (string.length / 2)
+    let l = -1;
+    let r = -1;
+
+    if (string.length % 2 != 0) {
+        l = middle;
+        r = middle;
+    }
+    else {
+        l = middle - 1;
+        r = middle;
+    }
+    l = Math.floor(l)
+    r = Math.floor(r)
+    console.log(l)
+    console.log(r)
+    while (l >= 0 && r < string.length) {
+        if (string[l] != string[r]) {
+            return false;
+        }
+        l -= 1
+        r += 1
+    }
+
+    return true
 
 };
 
